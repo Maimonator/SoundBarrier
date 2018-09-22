6c055d44a27af6020a3b8f8469ac6899f0d72b1e
Maimonator <tal161195@gmail.com>
2018-09-22 16:27:25 +0300

    soundplot: add harmonic and percussive plot support

--- main.py
+++ main.py
@@ -18,6 +18,37 @@ import librosa
 import librosa.display
 
 
+class SoundPlot(object):
+    """docstring for SoundPlot"""
+    COLORBAR_FORMAT_DB = '%+02.0f dB'
+
+    def __init__(self):
+        super(SoundPlot, self).__init__()
+
+    def __enter__(self):
+        self.fig = plt.figure(figsize=(12, 6))
+        self.subplot_index = 1
+        return self
+
+    def append(self, title, data, samplerate,
+               x_axis='time', y_axis='mel', colorbar_format=None):
+
+        self.fig.add_subplot(2, 1, self.subplot_index)
+        self.subplot_index += 1
+
+        librosa.display.specshow(data, sr=samplerate,
+                                 x_axis=x_axis, y_axis=y_axis)
+        plt.title(title)
+        plt.colorbar(format=colorbar_format)
+
+    def save_svg(self, output):
+        plt.tight_layout()
+        self.fig.savefig(output)
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        plt.close(self.fig)
+
+
 class SoundBarrier(object):
     """docstring for SoundBarrier"""
 
@@ -38,14 +69,33 @@ class SoundBarrier(object):
         print self.samplerate
         return tempo
 
+    def get_song_graph(self):
+        ats_harmonic, ats_percussive = librosa.effects.hpss(
+            self.audio_time_series)
+
+        # Convert to log scale (db).
+        mel_harmonic = librosa.feature.melspectrogram(ats_harmonic)
+        mel_percussive = librosa.feature.melspectrogram(ats_percussive)
+        log_harmonic = librosa.power_to_db(mel_harmonic, ref=np.max)
+        log_percussive = librosa.power_to_db(mel_percussive, ref=np.max)
+
+        with SoundPlot() as sp:
+            sp.append('{} Harmonic'.format(self.filename), log_harmonic,
+                      self.samplerate,
+                      colorbar_format=SoundPlot.COLORBAR_FORMAT_DB)
+            sp.append('{} Percussive'.format(self.filename), log_percussive,
+                      self.samplerate,
+                      colorbar_format=SoundPlot.COLORBAR_FORMAT_DB)
+            sp.save_svg(os.path.join(self.output, self.filename + ".svg"))
+
     def __eq__(self, other):
         return self.get_bpm() == other.get_bpm()
 
 
 def get_song_info(args):
     sb = SoundBarrier(args.input, args.output)
-    print "BPM of the song is {}".format(sb.get_bpm())
-
+    # print "the BPM of the song {} is {}".format(sb.filename, sb.get_bpm())
+    sb.get_song_graph()
 
 
 def main():
