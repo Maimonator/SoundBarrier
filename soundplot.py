6502813651175a9880bf3ae222b0616c61df7b3a
Maimonator <tal161195@gmail.com>
2018-09-23 15:36:56 +0300

    soundplot: add implementation from PlotContainer
    now SoundPlot doesn't receive random values but a PlotContainer
    for any value that isn't specified in PlotContainer, you can pass it to librosa.display.specshow
    by using the kwargs

--- soundplot.py
+++ soundplot.py
@@ -5,14 +5,26 @@ import librosa.display
 ms.use('seaborn-muted')
 
 
+class PlotContainer(object):
+    """docstring for PlotContainer"""
+
+    def __init__(self, title, data, colorbar_format=None, **kwargs):
+        super(PlotContainer, self).__init__()
+        self.title = title
+        self.data = data
+        self.colorbar_format = colorbar_format
+        self.kwargs = kwargs
+
+
 class SoundPlot(object):
     """docstring for SoundPlot"""
     COLORBAR_FORMAT_DB = '%+02.0f dB'
     MAX_NUMBER_OF_PLOTS = 10
 
-    def __init__(self, num_of_rows, fig_width=6, fig_height=6):
+    def __init__(self, num_of_rows, samplerate, fig_width=6, fig_height=6):
         super(SoundPlot, self).__init__()
         self.num_of_rows = num_of_rows
+        self.samplerate = samplerate
         self.fig_width = fig_width
         self.fig_height = fig_height
 
@@ -21,8 +33,7 @@ class SoundPlot(object):
         self.subplot_index = 1
         return self
 
-    def append(self, title, data, samplerate,
-               x_axis='time', y_axis='mel', colorbar_format=None, **kwargs):
+    def append(self, plot_obj):
 
         self.fig.set_size_inches(self.fig_width,
                                  self.subplot_index * self.fig_height)
@@ -30,11 +41,11 @@ class SoundPlot(object):
         subplot = self.fig.add_subplot(self.num_of_rows, 1, self.subplot_index)
         self.subplot_index += 1
 
-        librosa.display.specshow(data, sr=samplerate,
-                                 x_axis=x_axis, y_axis=y_axis, **kwargs)
+        librosa.display.specshow(
+            plot_obj.data, sr=self.samplerate, **plot_obj.kwargs)
 
-        subplot.title.set_text(title)
-        plt.colorbar(format=colorbar_format)
+        subplot.title.set_text(plot_obj.title)
+        plt.colorbar(format=plot_obj.colorbar_format)
 
     def save_svg(self, output):
         plt.tight_layout()
