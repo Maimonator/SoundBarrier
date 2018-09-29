616922fbff73f06220ac111349ec7430c50a89f1
Maimonator <tal161195@gmail.com>
2018-09-29 18:02:24 +0300

    soundbarrier: add amplitude plot generation

--- soundbarrier.py
+++ soundbarrier.py
@@ -2,7 +2,68 @@ import os
 import numpy as np
 import librosa
 
-from soundplot import SoundPlot, PlotContainer
+from soundplot import SoundPlot, DataPlot
+
+
+def smooth(x, window_len=11, window='hanning'):
+    """Taken from https://scipy-cookbook.readthedocs.io/items/SignalSmooth.html
+    smooth the data using a window with requested size.
+
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+
+    input:
+        x: the input signal
+        window_len: the dimension of the smoothing window;
+        should be an odd integer window: the type of
+        window from 'flat', 'hanning',
+        'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+
+    see also:
+
+    numpy.hanning, numpy.hamming, numpy.bartlett,
+    numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+
+    TODO: the window parameter could be the window
+    itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct
+    this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError("smooth only accepts 1 dimension arrays.")
+
+    if x.size < window_len:
+        raise ValueError("Input vector needs to be bigger than window size.")
+
+    if window_len < 3:
+        return x
+
+    if window not in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError("Window is on of 'flat', 'hanning', \
+         'hamming', 'bartlett', 'blackman'")
+
+    s = np.r_[x[window_len - 1:0:-1], x, x[-2:-window_len - 1:-1]]
+    if window == 'flat':  # moving average
+        w = np.ones(window_len, 'd')
+    else:
+        w = eval('np.' + window + '(window_len)')
+
+    y = np.convolve(w / w.sum(), s, mode='valid')
+    return y
 
 
 class SoundBarrier(object):
@@ -10,6 +71,7 @@ class SoundBarrier(object):
     PLOT_PREFIX_PERCUSSIVE = "percussive"
     PLOT_PREFIX_HARMONIC = "harmonic"
     PLOT_PREFIX_CHROMA = "chroma"
+    PLOT_PREFIX_AMP = "amplitude"
 
     NUM_OF_GRAPHS = 3
 
@@ -93,11 +155,27 @@ class SoundBarrier(object):
             x_coords=librosa.frames_to_time(fixed_beats))
         return plot_obj
 
+    def get_amp_plot(self):
+        onset_env = librosa.onset.onset_strength(y=self.ats_percussive,
+                                                 sr=self.samplerate,
+                                                 aggregate=np.median)
+
+        smoothed_onset = smooth(onset_env, (len(onset_env) / 20) + 1)
+
+        plot_obj = DataPlot('{} Amplitude Graph'.format(self.filename),
+                            smoothed_onset,
+                            self.get_plot_output_path(
+                                SoundBarrier.PLOT_PREFIX_AMP),
+                            "Amplitude")
+
+        return plot_obj
+
     def get_song_graph(self):
         plots = []
         plots.append(self.get_percussive_plot())
         plots.append(self.get_harmonic_plot())
         plots.append(self.get_chroma_plot())
+        plots.append(self.get_amp_plot())
 
         outputs = []
         for plot in plots:
