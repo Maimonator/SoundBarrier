870c0248abc3aeaf408871862dfd16db139b62c7
Maimonator <tal161195@gmail.com>
2018-10-01 02:45:37 +0300

    videobarrier: add clip editing feature with info from soundbarrier

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 from soundbarrier import SoundBarrierItem
+from videobarrier import VideoBarrier
 from glob import glob
 from itertools import chain
 import logging
@@ -23,12 +24,29 @@ def compare_song(args):
         for path in args.input:
             with SoundBarrierItem(path, args.output) as sb1:
                 print "====================="
-                score, frame_diff = sb1.compare_to(sb2)
+                score, time_diff = sb1.compare_to(sb2)
                 print """
-                For a song score of {}, song should start at {}(s) """.format(
+For a song score of {}, song should start at {}(s) """.format(
                     score,
-                    frame_diff)
+                    time_diff)
                 print "---------------------"
+
+
+def create_video(args):
+    with SoundBarrierItem(args.song, args.output) as sb2:
+        for path in args.input:
+            with SoundBarrierItem(path, args.output) as sb1:
+                print "====================="
+                score, time_diff = sb1.compare_to(sb2)
+                print """
+For a song score of {}, song should start at {}(s) """.format(
+                    score,
+                    time_diff)
+                print "---------------------"
+
+    with VideoBarrier(args.clip, args.output, args.song) as vb:
+        vb.set_audio(time_diff, time_diff + args.duration)
+        vb.save(args.clip_start_time, args.duration)
 
 
 def flat_file_list(l):
@@ -50,6 +68,8 @@ this program analyzes music woot woot.
         default="generated")
 
     subparsers = parser.add_subparsers()
+
+    # Info parser
     info_parser = subparsers.add_parser(
         'info',
         help="return info on the song analyzed")
@@ -68,6 +88,7 @@ this program analyzes music woot woot.
 
     info_parser.set_defaults(do=get_song_info)
 
+    # Diff Parser
     diff_parser = subparsers.add_parser(
         'diff',
         help="gives a score between 0 to 1 on how similar the songs are")
@@ -78,6 +99,36 @@ this program analyzes music woot woot.
         help="song path to compare to", required=True)
 
     diff_parser.set_defaults(do=compare_song)
+
+    # Video Parser
+    video_parser = subparsers.add_parser(
+        'video',
+        help="Edits a song into another video")
+
+    video_parser.add_argument(
+        '-s',
+        '--song',
+        help="song path to edit into the clip", required=True)
+
+    video_parser.add_argument(
+        '--clip',
+        help="video clip path to edit", required=True)
+
+    video_parser.add_argument(
+        '--clip-start-time',
+        help="where to start the clip in seconds",
+        type=float,
+        default=0)
+
+    video_parser.add_argument(
+        '-d',
+        '--duration',
+        help="clip duration in seconds",
+        type=float,
+        default=60.0)
+
+    video_parser.set_defaults(do=create_video)
+
     args = parser.parse_args()
     args.input = flat_file_list(args.input)
     args.do(args)
